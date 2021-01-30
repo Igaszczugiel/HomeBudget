6cc26eec87e4972d18b1e9ebcd136358f635dbaf
Igaszczugiel <78268100+Igaszczugiel@users.noreply.github.com>
2021-01-30 15:40:01 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+
+// <!-- JS SHOW / 01 suma przychodow minus suma wydatkow, id="Fund" -->
+// 1. Zaciagnac dane z htmla body DOM (span o id="Fund")
+// 2. w elemencie - spanie - o id="Fund" ma pojawic sie tresc
+// 3. Tresc, to ma byc efekt dzialania funkcji, ktora od elementu id="IncomeSum" odejmie element id="OutcomeSum"
+
+// let FundAvailable = document.getElementById('Fund');
+// let body = document.querySelector("body");
+// function fillElement(parametr) {
+//     const span = document.createElement("span");
+//     span.innerText = parametr;
+//     return span;
+//   };
+  
+//   fillElement("33");
+
+
+// 
+// INCOMES
+// 
+// <!-- JS SHOW / 02 suma przychodow id="IncomeSum" -->
+// 1. Zsumowac wszystkie elementy pochodzace z inputow 
+
+
+// <!-- JS INPUT / 04 przychod id="IncomeAmount" -->
+// Element ma zostac przekazany i wlaczony do tresci DOM jako element listy, ma sie pojawic tuz obok id="IncomeName"
+
+
+// <!-- JS INPUT / 06 nazwa przychodu id="IncomeName" -->
+// Element ma zostac przekazany i wlaczony do tresci DOM jako element listy 
+
+
+// <!-- JS INPUT / 08 eventListener on click? Ten button dodaje elementy id="IncomeName" oraz id="IncomeAmount"
+//  do drzewa DOM ponizej / id="addIncomeNameAndAmount"  -->
+
+
+
+
+// 
+// 
+// OUTCOMES
+// 
+// <!-- JS SHOW / 03 suma kosztow id="OutcomeSum" -->
+// Funkcja, ktora zsumuje wszystkie inputy / tablice?
+
+
+// <!-- JS INPUT / 05 przychod id="OutcomeAmount" -->
+// Element ma zostac przekazany i wlaczony do tresci DOM jako element listy, ma sie pojawic tuz obok id="OutcomeName" 
+
+
+// <!-- JS INPUT / 07 nazwa przychodu id="OutcomeName" -->
+// Element ma zostac przekazany i wlaczony do tresci DOM jako element listy 
+
+
+// <!-- JS INPUT / 09 eventListener on click? Ten button dodaje elementy id="OutcomeName" oraz id="OutcomeAmount"
+//  do drzewa DOM ponizej / id="addOutcomeNameAndAmount"  -->
+
+
