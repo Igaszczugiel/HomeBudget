9576dacb1ca7d854a3e58159273f5530002058b9
Igaszczugiel <78268100+Igaszczugiel@users.noreply.github.com>
2021-02-11 18:02:11 +0000

    updated - under construction

--- index.js
+++ index.js
@@ -1,58 +1,64 @@
+var incomingsList = document.querySelector('#incomingsList')
+var addIncoming = document.querySelector('#addIncoming')
+var state = {
+  incomings: [
+    {name: "A", amount: 2050},
+    {name: "B", amount: 3000},
+  ],
+  outgoings: []
+}
 
-// <!-- JS SHOW / 01 suma przychodow minus suma wydatkow, id="Fund" -->
-// 1. Zaciagnac dane z htmla body DOM (span o id="Fund")
-// 2. w elemencie - spanie - o id="Fund" ma pojawic sie tresc
-// 3. Tresc, to ma byc efekt dzialania funkcji, ktora od elementu id="IncomeSum" odejmie element id="OutcomeSum"
+renderApp()
 
-// let FundAvailable = document.getElementById('Fund');
-// let body = document.querySelector("body");
-// function fillElement(parametr) {
-//     const span = document.createElement("span");
-//     span.innerText = parametr;
-//     return span;
-//   };
+function renderApp() {
+  renderUI()
+  renderSumUI()
+}
+
+
+
+
+
+function renderUI() {
+  incomingsList.innerHTML = ""
   
-//   fillElement("33");
+  state.incomings.forEach(item => {
+    var liInnerHTML = `
+        <li data-name="${item.name}">
+          ${item.name} - ${item.amount}zl 
+          <button>Edytuj</button> 
+          <button>Usun</button>
+        </li>
+    `
+    
+    var newLi = document.createElement("li")
+    newLi.innerHTML = liInnerHTML
+    incomingsList.append(newLi)
+  })
+}
+
+function renderSumUI() {
+  var incomingsSum = document.querySelector('#incomingsSum')
+  incomingsSum.innerHTML = `suma przychodow: ${sum(state.incomings)}zl`
+  // TODO: outgoingsSum update
+}
+
+function sum(arr) {
+  return arr.reduce((acc, item) => acc + item.amount, 0)
+}
 
 
-// 
-// INCOMES
-// 
-// <!-- JS SHOW / 02 suma przychodow id="IncomeSum" -->
-// 1. Zsumowac wszystkie elementy pochodzace z inputow 
+// ADD NEW ITEM
 
+addIncoming.addEventListener('click', addNewItem)
 
-// <!-- JS INPUT / 04 przychod id="IncomeAmount" -->
-// Element ma zostac przekazany i wlaczony do tresci DOM jako element listy, ma sie pojawic tuz obok id="IncomeName"
-
-
-// <!-- JS INPUT / 06 nazwa przychodu id="IncomeName" -->
-// Element ma zostac przekazany i wlaczony do tresci DOM jako element listy 
-
-
-// <!-- JS INPUT / 08 eventListener on click? Ten button dodaje elementy id="IncomeName" oraz id="IncomeAmount"
-//  do drzewa DOM ponizej / id="addIncomeNameAndAmount"  -->
-
-
-
-
-// 
-// 
-// OUTCOMES
-// 
-// <!-- JS SHOW / 03 suma kosztow id="OutcomeSum" -->
-// Funkcja, ktora zsumuje wszystkie inputy / tablice?
-
-
-// <!-- JS INPUT / 05 przychod id="OutcomeAmount" -->
-// Element ma zostac przekazany i wlaczony do tresci DOM jako element listy, ma sie pojawic tuz obok id="OutcomeName" 
-
-
-// <!-- JS INPUT / 07 nazwa przychodu id="OutcomeName" -->
-// Element ma zostac przekazany i wlaczony do tresci DOM jako element listy 
-
-
-// <!-- JS INPUT / 09 eventListener on click? Ten button dodaje elementy id="OutcomeName" oraz id="OutcomeAmount"
-//  do drzewa DOM ponizej / id="addOutcomeNameAndAmount"  -->
-
-
+function addNewItem(e) {
+  e.preventDefault()
+  
+  var newName = document.querySelector('#newName').value
+  var newAmount = Number(document.querySelector('#newAmount').value)
+  var newItem = {name: newName, amount: newAmount}
+  state.incomings.push(newItem) //update of data (danych)
+  
+  renderApp()
+}
